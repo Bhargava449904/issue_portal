c11168e00db74982a0f9ab3f6da5074dc3117f73
dasari bhargava <bhargava449904>
2026-02-06 19:10:35 +0530

    status update issue

--- issues/urls.py
+++ issues/urls.py
@@ -7,7 +7,7 @@ urlpatterns=[
     path("view_my_issues/",view=views.view_my_issues),
     path("user_get_issues/",view=views.user_get_issues),
     path("admin_view_all_issues/",view=views.admin_view_all_issues),
-    path("admin_update_issue_status/<int:issue_id>",view=views.admin_update_issue_status),
+    path("admin_update_issue_status/<int:issue_id>/",view=views.admin_update_issue_status),
     path("admin_delete_issue/<int:issue_id>",view=views.admin_delete_issue),
     
 ]
