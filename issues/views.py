c9580bf52aa95d0116d845292f06adde20e1e791
dasari bhargava <bhargava449904>
2026-02-06 18:37:17 +0530

    status update issue

--- issues/views.py
+++ issues/views.py
@@ -7,6 +7,7 @@ import cloudinary
 import cloudinary.uploader
 import bcrypt
 from acoounts.models import User
+import json
 # Create your views here.
 
 def hello(request):
@@ -135,44 +136,66 @@ def admin_view_all_issues(request):
 
 @csrf_exempt
 def admin_update_issue_status(request, issue_id):
-    # 1️⃣ Allow only PUT
+
     if request.method != "POST":
         return JsonResponse({"error": "POST method required"}, status=405)
 
-    # 2️⃣ Authenticate admin
+    # authenticate admin
     user = get_user_from_token(request)
     if not user or user["role"] != "admin":
         return JsonResponse({"error": "Admin only"}, status=403)
 
-    # 3️⃣ Get new status
-    new_status = request.POST.get("status")
+    try:
+        body = json.loads(request.body)
+        new_status = body.get("status")
+    except json.JSONDecodeError:
+        return JsonResponse({"error": "Invalid JSON"}, status=400)
 
     allowed_status = ["pending", "in_progress", "resolved"]
     if new_status not in allowed_status:
-        return JsonResponse(
-            {"error": "Invalid status value"},
-            status=400
-        )
+        return JsonResponse({"error": "Invalid status"}, status=400)
 
-    # 4️⃣ Get issue
     try:
         issue = Issue.objects.get(id=issue_id)
     except Issue.DoesNotExist:
         return JsonResponse({"error": "Issue not found"}, status=404)
 
-    # 5️⃣ Update status
     issue.status = new_status
     issue.save()
 
-    # 6️⃣ Success response
-    return JsonResponse(
-        {
-            "message": "Issue status updated successfully",
-            "issue_id": issue.id,
-            "new_status": issue.status
-        },
-        status=200
-    )
+    return JsonResponse({
+        "message": "Status updated successfully",
+        "issue_id": issue.id,
+        "status": issue.status
+    }, status=200)
+
+
+
+@csrf_exempt
+def user_get_issues(request):
+
+    if request.method != "GET":
+        return JsonResponse({"error": "GET method required"}, status=405)
+
+    user = get_user_from_token(request)
+    if not user:
+        return JsonResponse({"error": "Unauthorized"}, status=401)
+
+    issues = Issue.objects.filter(user_id=user["id"]).order_by("-id")
+
+    data = []
+    for issue in issues:
+        data.append({
+            "id": issue.id,
+            "title": issue.title,
+            "description": issue.description,
+            "status": issue.status,
+            "created_at": issue.created_at,
+        })
+
+    return JsonResponse({
+        "issues": data
+    }, status=200)
 
 # admin can delete the issue 
 
